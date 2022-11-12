b0a234d299d0fc19629b5c8a1c356ee5c101b980
unknown <angelaescariocorpuz@gmail.com>
2022-11-12 12:00:10 +0800

    implemented code for the menu of the program and did some changes on the program intro

--- Contact Tracing.py
+++ Contact Tracing.py
@@ -9,17 +9,31 @@ from colored import stylize
 import time 
 
 # write program intro
-print (stylize("══════════════════════════════ CONTACT TRACING ════════════════════════════════════════", colored.fg ('indian_red_1a')))
+header = "══════════════════════════════ CONTACT TRACING ════════════════════════════════════════"
+print (stylize(header, colored.fg ('indian_red_1a')))
 print ()
 time.sleep (1)
 print ("A menu will appear where you can choose to add your information and search for it after.")
 print ("For the details needed for contact tracing, please input all the information being asked.")
 print ()
-print (stylize("═══════════════════════════════════════════════════════════════════════════════════════", colored.fg ('indian_red_1a')))
+time.sleep (1)
+print (stylize("═" * len(header), colored.fg ('indian_red_1a')))
 print ()
 print ()
+time.sleep (1)
 
 # display menu
+header_menu = "═════════════════════════════ CONTACT TRACING MENU ════════════════════════════════════"
+print (stylize (header_menu, colored.fg ('pale_green_3b')))
+print()
+time.sleep (1)
+print ("               1 --> Add a contact tracing information")
+print ("               2 --> Search for person's contact tracing information")
+print ("               3 --> Exit program")
+print ()
+time.sleep (1)
+print (stylize ("═" * len(header_menu), colored.fg ('pale_green_3b')))
+time.sleep (1)
 
 # ask user input
 # create option 1: add items
