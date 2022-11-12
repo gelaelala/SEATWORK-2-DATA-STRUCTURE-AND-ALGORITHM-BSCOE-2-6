83d85f3cf84e936ecf9cb861ae47816eb32971b5
unknown <angelaescariocorpuz@gmail.com>
2022-11-12 11:27:24 +0800

    wrote pseudo code for the program

---
+++ Contact Tracing.py
@@ -0,0 +1,12 @@
+# Seatwork 2
+# - write a python program for contact tracing
+# key: full name
+# value: another dictionary of personal information
+
+# import modules
+# write program intro
+# display menu
+# ask user input
+# create option 1: add items
+# create option 2: search for item (full name)
+# create option 3: exit program
