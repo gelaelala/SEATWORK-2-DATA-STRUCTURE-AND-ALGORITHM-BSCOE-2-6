2445bddb0908ca77b4b6bbf81c2b94a4d8f58437
unknown <angelaescariocorpuz@gmail.com>
2022-11-12 11:44:00 +0800

    implemented the program intro

--- Contact Tracing.py
+++ Contact Tracing.py
@@ -9,7 +9,18 @@ from colored import stylize
 import time 
 
 # write program intro
+print (stylize("══════════════════════════════ CONTACT TRACING ════════════════════════════════════════", colored.fg ('indian_red_1a')))
+print ()
+time.sleep (1)
+print ("A menu will appear where you can choose to add your information and search for it after.")
+print ("For the details needed for contact tracing, please input all the information being asked.")
+print ()
+print (stylize("═══════════════════════════════════════════════════════════════════════════════════════", colored.fg ('indian_red_1a')))
+print ()
+print ()
+
 # display menu
+
 # ask user input
 # create option 1: add items
 # create option 2: search for item (full name)
